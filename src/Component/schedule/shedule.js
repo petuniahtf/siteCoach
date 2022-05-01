06b4f169f04f48389d4a988f0a85b7526d12fc91
petuniahtf <priscilliaweber6@hotmail.com>
2022-05-01 03:38:42 +0200

    skeleton

---
+++ src/Component/schedule/shedule.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './shedule.css'
+const Shedule = () =>{
+
+    return(
+        <></>
+    )
+}
+
+export default Shedule;
---
+++ src/Component/selectDetail/selectDetail.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './selectDetail.css'
+const SelectDetail = () =>{
+
+    return(
+        <></>
+    )
+}
+
+export default SelectDetail;
